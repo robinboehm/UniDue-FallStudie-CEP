10f96364b3c1e106228cd082a481a1d47918d5a5
Stephen Said <stephen.said@hp.com>
2012-10-13 16:32:32 +0200

    ..

--- RouteGenerator/src/fl/routeGenerator/RouteGenerator.java
+++ RouteGenerator/src/fl/routeGenerator/RouteGenerator.java
@@ -1,20 +1,26 @@
 package fl.routeGenerator;
 
+import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 import javax.swing.JTextField;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreeNode;
 
 import org.jdesktop.swingx.JXMapKit;
 import org.jdesktop.swingx.JXMapKit.DefaultProviders;
@@ -48,50 +54,80 @@ public class RouteGenerator extends JFrame {
 	private JButton clear;
 
 	public RouteGenerator() {
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+
+		// Frame
 		this.setTitle("Banana Route Generator");
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.getContentPane().setLayout(new FlowLayout(FlowLayout.LEFT));
+		this.getContentPane().setLayout(new GridBagLayout());
 
 		// Maps
+		c.gridx = 0;
+		c.gridy = 0;
 		this.maps = new JXMapKit();
 		this.maps.setDefaultProvider(DefaultProviders.OpenStreetMaps);
 		this.maps.setDataProviderCreditShown(true);
 		this.maps.setPreferredSize(new Dimension(800, 600));
 		this.maps.setVisible(true);
+		this.maps.setBorder(BorderFactory.createLineBorder(Color.black));
 		this.maps.getMainMap().addMouseListener(new MapMouseListener(this));
-		this.getContentPane().add(maps);
-		customPainter = new CompoundPainter<WaypointPainter<JXMapViewer>>();
-		routePainter = new RoutePainter<JXMapViewer>();
-		customPainter.setPainters(routePainter);
-		customPainter.setCacheable(false);
-		maps.getMainMap().setOverlayPainter(customPainter);
+		this.getContentPane().add(maps, c);
+		this.customPainter = new CompoundPainter<WaypointPainter<JXMapViewer>>();
+		this.routePainter = new RoutePainter<JXMapViewer>();
+		this.customPainter.setPainters(routePainter);
+		this.customPainter.setCacheable(false);
+		this.maps.getMainMap().setOverlayPainter(customPainter);
 
 		// Other UI
+		// Route Tree
 		JPanel treePanel = new JPanel();
 		treePanel.setLayout(new BoxLayout(treePanel, BoxLayout.Y_AXIS));
-		this.getContentPane().add(treePanel);
-
 		this.routeTree = new JTree();
-		this.routeTree.setModel(null);
+		this.routeTree.setModel(new DefaultTreeModel(
+				new DefaultMutableTreeNode("Route")));
 		this.routeTree.setPreferredSize(new Dimension(250, 300));
+		this.routeTree.addKeyListener(new TreeKeyListener(this));
 		JScrollPane jsp = new JScrollPane(this.routeTree);
 		treePanel.add(jsp);
 
-		this.routeName = new JTextField();
-		treePanel.add(this.routeName);
+		// Buttons
+		JPanel otherPanel = new JPanel();
+		otherPanel.setLayout(new GridBagLayout());
 
+		c.gridx = 0;
+		c.gridy = 0;
+		c.gridwidth = 2;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		this.routeName = new JTextField();
+		otherPanel.add(this.routeName, c);
+
+		c.gridx = 0;
+		c.gridy = 1;
+		c.gridwidth = 1;
 		this.clear = new JButton("Clear route");
 		this.clear.setName("clear");
 		this.clear.addMouseListener(new DefaultMouseListener(this));
-		treePanel.add(this.clear);
+		otherPanel.add(this.clear, c);
 
-		this.save = new JButton("Create new route");
+		c.gridx = 1;
+		c.gridy = 1;
+		this.save = new JButton("Save route to COSM");
 		this.save.setName("save");
 		this.save.addMouseListener(new DefaultMouseListener(this));
-		treePanel.add(this.save);
+		otherPanel.add(this.save, c);
+
+		// Split Pane
+		c.gridx = 1;
+		c.gridy = 0;
+		c.fill = GridBagConstraints.BOTH;
+		JSplitPane routePanel = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+		routePanel.add(treePanel);
+		routePanel.add(otherPanel);
+		this.getContentPane().add(routePanel, c);
 
 		// Other
-		this.route = new TreeSet<Waypoint>();
+		this.route = new LinkedHashSet<Waypoint>();
 
 		// Finally
 		this.pack();
@@ -126,10 +162,8 @@ public class RouteGenerator extends JFrame {
 		Iterator<Waypoint> i = this.route.iterator();
 		while (i.hasNext()) {
 			RoutePoint current = (RoutePoint) i.next();
-			DefaultMutableTreeNode node = new DefaultMutableTreeNode(current
-					.getPosition().getLatitude()
-					+ ";"
-					+ current.getPosition().getLongitude());
+			DefaultMutableTreeNode node = new DefaultMutableTreeNode(
+					getCoordinateString(current));
 			root.add(node);
 		}
 
@@ -154,8 +188,7 @@ public class RouteGenerator extends JFrame {
 					routePoint.setId(j);
 					routePoint.setMinValue(-30d);
 					routePoint.setMaxValue(-50d);
-					routePoint.setTag(current.getPosition().getLatitude() + ";"
-							+ current.getPosition().getLongitude());
+					routePoint.setTag(getCoordinateString(current));
 					routeFeed.addData(routePoint);
 					j++;
 				}
@@ -167,10 +200,40 @@ public class RouteGenerator extends JFrame {
 		}
 	}
 
+	private String getCoordinateString(RoutePoint current) {
+		return current.getPosition().getLatitude() + ";"
+				+ current.getPosition().getLongitude();
+	}
+
 	public void clear() {
 		this.route.clear();
 		this.routeName.setText("");
+		this.updateMapWithTree();
+	}
+
+	private void updateMapWithTree() {
 		this.updateTree();
 		this.repaintMap();
+	}
+
+	public void deleteRoutePoint() {
+		if (this.routeTree.getSelectionPath().getLastPathComponent() != null) {
+			String point = ((TreeNode) this.routeTree.getSelectionPath()
+					.getLastPathComponent()).toString();
+
+			((DefaultTreeModel) this.routeTree.getModel())
+					.removeNodeFromParent((MutableTreeNode) this.routeTree
+							.getSelectionPath().getLastPathComponent());
+
+			Iterator<Waypoint> i = this.route.iterator();
+			while (i.hasNext()) {
+				RoutePoint current = (RoutePoint) i.next();
+
+				if (getCoordinateString(current).equals(point))
+					i.remove();
+			}
+
+			this.updateMapWithTree();
+		}
 	}
 }
---
+++ RouteGenerator/src/fl/routeGenerator/TreeKeyListener.java
@@ -0,0 +1,27 @@
+package fl.routeGenerator;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class TreeKeyListener implements KeyListener {
+
+	private RouteGenerator routeGenerator;
+
+	public TreeKeyListener(RouteGenerator routeGenerator) {
+		this.routeGenerator = routeGenerator;
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		if (arg0.getKeyCode() == KeyEvent.VK_DELETE)
+			this.routeGenerator.deleteRoutePoint();
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+	}
+}
