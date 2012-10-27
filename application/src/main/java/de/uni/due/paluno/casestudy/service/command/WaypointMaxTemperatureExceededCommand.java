934db399ed768a9a3b4c816ff0b1f9e520e2536b
saids <saids@SAIDS.emea.hpqcorp.net>
2012-10-27 20:49:00 +0200

    Some Missing classes

---
+++ application/src/main/java/de/uni/due/paluno/casestudy/service/command/WaypointMaxTemperatureExceededCommand.java
@@ -0,0 +1,52 @@
+package de.uni.due.paluno.casestudy.service.command;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.espertech.esper.client.EPRuntime;
+
+import de.uni.due.paluno.casestudy.Globals;
+import de.uni.due.paluno.casestudy.cep.events.RouteEvent;
+import de.uni.due.paluno.casestudy.cep.events.WaypointTemperatureEvent;
+import de.uni.due.paluno.casestudy.model.Route;
+
+public class WaypointMaxTemperatureExceededCommand extends RouteEventCommand {
+
+	public WaypointMaxTemperatureExceededCommand(Route route) {
+		super(route);
+	}
+
+	@Override
+	protected void executeLogic(EPRuntime epr, Map<String, Object> eventParams) {
+		RouteEvent routeEvent = new RouteEvent();
+		routeEvent.setTarget(route.getId());
+		routeEvent.setKey(Globals.E_EVENT_WAYPOINT_MAX_EXCEEDED);
+
+		epr.sendEvent(routeEvent);
+	}
+
+	@Override
+	public String getEPL() {
+		String epl = "select count(data) as " + this.getColumns()[0] + " from "
+				+ Globals.E_TEMPERATURE_ENTITY + " where target in ("
+				+ getWaypoints() + ") having data > 40";
+
+		return epl;
+	}
+
+	@Override
+	public Map<String, String> getEventTypes() {
+		Map<String, String> eventTypes = new HashMap<String, String>();
+
+		eventTypes.put(Globals.E_TEMPERATURE_ENTITY,
+				WaypointTemperatureEvent.class.getName());
+
+		return eventTypes;
+	}
+
+	@Override
+	public String[] getColumns() {
+		return null;
+	}
+
+}
