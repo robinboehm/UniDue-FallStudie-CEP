d8613ed76b78b62ddc49512c0c4cb94a517f72c9
Max <mczheinemeyer@gmx.de>
2012-11-13 13:34:05 +0100

    implemented cleaning function of tableUIElement properly

--- application/src/main/webapp/js/tableUIElement.js
+++ application/src/main/webapp/js/tableUIElement.js
@@ -22,7 +22,7 @@ function fillTablesUIElement(transports) {
 }
 
 function clearTablesUIElement() {
-	$('#truckTable').dataTable().fnClearData();
+	$('#truckTable').dataTable().fnClearTable();
 }
 
 function createTableComponent() {
